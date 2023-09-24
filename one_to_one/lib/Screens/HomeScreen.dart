276b8f334c68162df337862c77024371757bd60d
deepak27rathore <deepak2003rathore@gmail.com>
2023-09-24 22:33:50 +0530

    .StreamBuilder to Load Data From Cloud Firestore Firebase

--- one_to_one/lib/Screens/HomeScreen.dart
+++ one_to_one/lib/Screens/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -16,35 +18,52 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //appbar
-        appBar: AppBar(
-          title: const Text('chat'),
-          leading: const Icon(CupertinoIcons.home),
-          actions: [
-            //search user button
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+      //appbar
+      appBar: AppBar(
+        title: const Text('chat'),
+        leading: const Icon(CupertinoIcons.home),
+        actions: [
+          //search user button
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
 
-            //more features button
-            IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
-          ],
-        ),
-        //floating button to add new user button
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: FloatingActionButton(
-              onPressed: () async {
-                await APIs.auth.signOut();
-                await GoogleSignIn().signOut();
-              },
-              child: const Icon(Icons.add_comment_rounded)),
-        ) // AppBar
-        ,
-        body: ListView.builder(
-            itemCount: 16,
-            padding: EdgeInsets.only(top: mq.height * 0.01),
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              return const ChatUserCard();
-            })); // Scaffold
+          //more features button
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+      //floating button to add new user button
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+            onPressed: () async {
+              await APIs.auth.signOut();
+              await GoogleSignIn().signOut();
+            },
+            child: const Icon(Icons.add_comment_rounded)),
+      ) // AppBar
+      ,
+      //body
+      body: StreamBuilder(
+        stream: APIs.firestore.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          final list = [];
+
+          if (snapshot.hasData) {
+            final data = snapshot.data?.docs;
+            for (var i in data!) {
+              log('Data : ${i.data()}');
+              list.add(i.data()['Name']);
+            }
+          }
+          return ListView.builder(
+              itemCount: list.length,
+              padding: EdgeInsets.only(top: mq.height * 0.01),
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                //return const ChatUserCard()
+                return Text('Name: ${list[index]}');
+              });
+        },
+      ),
+    ); // Scaffold
   }
 }

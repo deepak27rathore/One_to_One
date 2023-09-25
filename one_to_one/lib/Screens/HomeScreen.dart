78cc0fd8ff5f137d9ed6ebad562748d2ca0da364
deepak27rathore <deepak2003rathore@gmail.com>
2023-09-26 04:06:45 +0530

    WillPopScope (Handles Back Button Click) & Hiding Keyboard on Tap

--- one_to_one/lib/Screens/HomeScreen.dart
+++ one_to_one/lib/Screens/HomeScreen.dart
@@ -31,103 +31,124 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //appbar
-      appBar: AppBar(
-        title: _isSearching
-            ? TextField(
-                decoration: const InputDecoration(
-                    border: InputBorder.none, hintText: 'Name,Email....'),
-                autofocus: true,
-                style: const TextStyle(fontSize: 17, letterSpacing: .5),
-                //when search  text changes updated search list
-                onChanged: (val) {
-                  //search logic
-                  _searchlist.clear();
-
-                  for (var i in _list) {
-                    if (i.name.toLowerCase().contains(val.toLowerCase()) ||
-                        i.name.toLowerCase().contains(val.toLowerCase())) {
-                      _searchlist.add(i);
-                      setState(() {
-                        _searchlist;
-                      });
-                    }
-                  }
-                },
-              )
-            : Text('chat'),
-        leading: const Icon(CupertinoIcons.home),
-        actions: [
-          //search user button
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  _isSearching = !_isSearching;
-                });
-              },
-              icon: Icon(_isSearching
-                  ? CupertinoIcons.clear_circled_solid
-                  : Icons.search)),
-
-          //more features button
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => ProfileScreen(user: APIs.me)));
-              },
-              icon: const Icon(Icons.more_vert))
-        ],
-      ),
-      //floating button to add new user button
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: FloatingActionButton(
-            onPressed: () async {
-              await APIs.auth.signOut();
-              await GoogleSignIn().signOut();
-            },
-            child: const Icon(Icons.add_comment_rounded)),
-      ) // AppBar
-      ,
-      //body
-      body: StreamBuilder(
-        stream: APIs.getAllUsers(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            //if data is loading
-            case ConnectionState.waiting:
-            case ConnectionState.none:
-              return const Center(child: CircularProgressIndicator());
-
-            //if some or more data is loaded then show it
-            case ConnectionState.active:
-            case ConnectionState.done:
-              final data = snapshot.data?.docs;
-              _list =
-                  data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
-
-              if (_list.isNotEmpty) {
-                return ListView.builder(
-                    itemCount: _isSearching ? _searchlist.length : _list.length,
-                    padding: EdgeInsets.only(top: mq.height * 0.01),
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return ChatUserCard(
-                          user:
-                              _isSearching ? _searchlist[index] : _list[index]);
-                      //return Text('Name: ${list[index]}');
-                    });
-              } else {
-                return const Center(
-                  child: Text("No Connections found !",
-                      style: TextStyle(fontSize: 20)),
-                );
-              }
+    return GestureDetector(
+      //for hiding keyboard
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: WillPopScope(
+        //if saerch is on & back button is pressed then close search
+        //or else close current screen on back button click
+        onWillPop: () {
+          if (_isSearching) {
+            setState(() {
+              _isSearching = !_isSearching;
+            });
+            return Future.value(false);
+          } else {
+            return Future.value(true);
           }
         },
+        child: Scaffold(
+          //appbar
+          appBar: AppBar(
+            title: _isSearching
+                ? TextField(
+                    decoration: const InputDecoration(
+                        border: InputBorder.none, hintText: 'Name,Email....'),
+                    autofocus: true,
+                    style: const TextStyle(fontSize: 17, letterSpacing: .5),
+                    //when search  text changes updated search list
+                    onChanged: (val) {
+                      //search logic
+                      _searchlist.clear();
+
+                      for (var i in _list) {
+                        if (i.name.toLowerCase().contains(val.toLowerCase()) ||
+                            i.name.toLowerCase().contains(val.toLowerCase())) {
+                          _searchlist.add(i);
+                          setState(() {
+                            _searchlist;
+                          });
+                        }
+                      }
+                    },
+                  )
+                : Text('chat'),
+            leading: const Icon(CupertinoIcons.home),
+            actions: [
+              //search user button
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isSearching = !_isSearching;
+                    });
+                  },
+                  icon: Icon(_isSearching
+                      ? CupertinoIcons.clear_circled_solid
+                      : Icons.search)),
+
+              //more features button
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => ProfileScreen(user: APIs.me)));
+                  },
+                  icon: const Icon(Icons.more_vert))
+            ],
+          ),
+          //floating button to add new user button
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: FloatingActionButton(
+                onPressed: () async {
+                  await APIs.auth.signOut();
+                  await GoogleSignIn().signOut();
+                },
+                child: const Icon(Icons.add_comment_rounded)),
+          ) // AppBar
+          ,
+          //body
+          body: StreamBuilder(
+            stream: APIs.getAllUsers(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                //if data is loading
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return const Center(child: CircularProgressIndicator());
+
+                //if some or more data is loaded then show it
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  final data = snapshot.data?.docs;
+                  _list =
+                      data?.map((e) => ChatUser.fromJson(e.data())).toList() ??
+                          [];
+
+                  if (_list.isNotEmpty) {
+                    return ListView.builder(
+                        itemCount:
+                            _isSearching ? _searchlist.length : _list.length,
+                        padding: EdgeInsets.only(top: mq.height * 0.01),
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return ChatUserCard(
+                              user: _isSearching
+                                  ? _searchlist[index]
+                                  : _list[index]);
+                          //return Text('Name: ${list[index]}');
+                        });
+                  } else {
+                    return const Center(
+                      child: Text("No Connections found !",
+                          style: TextStyle(fontSize: 20)),
+                    );
+                  }
+              }
+            },
+          ),
+        ),
       ),
     ); // Scaffold
   }

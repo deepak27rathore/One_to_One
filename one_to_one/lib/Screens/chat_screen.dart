ecf70fb2c77d1c5efcba903f4decb75949e0d98b
deepak27rathore <deepak2003rathore@gmail.com>
2023-09-26 14:04:15 +0530

    Chat App Text Input Field UI Design

--- one_to_one/lib/Screens/chat_screen.dart
+++ one_to_one/lib/Screens/chat_screen.dart
@@ -22,6 +22,11 @@ class _ChatScreenState extends State<ChatScreen> {
           automaticallyImplyLeading: false,
           flexibleSpace: _appBar(),
         ),
+
+        //body
+        body: Column(
+          children: [_chatInput()],
+        ),
       ),
     );
   }
@@ -61,9 +66,9 @@ class _ChatScreenState extends State<ChatScreen> {
             children: [
               Text(
                 widget.user.name,
-                style: TextStyle(
+                style: const TextStyle(
                     fontSize: 16,
-                    color: Colors.black54,
+                    color: Colors.black87,
                     fontWeight: FontWeight.w500),
               ),
               //for adding space
@@ -77,6 +82,81 @@ class _ChatScreenState extends State<ChatScreen> {
               )
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  //bottom chat input field
+  Widget _chatInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: mq.height * 0.01, horizontal: mq.width * 0.025),
+      child: Row(
+        children: [
+          //input field and buttons
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  //emoji button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.emoji_emotions,
+                        color: Color(0xFF4E74F9),
+                        size: 25,
+                      )),
+
+                  const Expanded(
+                      child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        hintText: 'Type Somethiong.....',
+                        hintStyle: TextStyle(color: Color(0xFF4E74F9)),
+                        border: InputBorder.none),
+                  )),
+
+                  //pick image from gallery  button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.image,
+                        color: Color(0xFF4E74F9),
+                        size: 26,
+                      )),
+
+                  //click image from camera  button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera_alt_rounded,
+                        color: Color(0xFF4E74F9),
+                        size: 26,
+                      )),
+                  //for adding space
+                  SizedBox(width: 2),
+                ],
+              ),
+            ),
+          ),
+
+          //send message button
+          MaterialButton(
+              onPressed: () {},
+              minWidth: 0,
+              padding: const EdgeInsets.only(
+                  top: 10, bottom: 10, right: 5, left: 10),
+              shape: const CircleBorder(),
+              color: Colors.green,
+              child: const Icon(
+                Icons.send,
+                color: Colors.white,
+                size: 28,
+              )),
         ],
       ),
     );
